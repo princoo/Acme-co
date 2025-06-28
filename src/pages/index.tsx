43b3d23a39d8ff14da5c66cb25435a56b39c5f1e
princoo <princeineza@gmail.com>
2025-06-28 14:04:17 +0200

    feat: simplify font imports and update className usage in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,13 @@
 import Hero from "@/components/Hero";
-import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 const plus_Jakarta_Sans = Plus_Jakarta_Sans({
   subsets: ["latin"],
 });
 
 export default function Home() {
   return (
-    <div className={`${geistSans.className} ${geistMono.className}${plus_Jakarta_Sans.className}`}>
+    <div className={`${plus_Jakarta_Sans.className}`}>
       <Hero />
     </div>
   );
